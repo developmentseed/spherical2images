975bcab8a81e5ddedf408fc2d1efb0cce9fe2d01
Rub21 <ruben@developmentseed.org>
2022-02-22 18:44:48 -0500

    Remove files

--- split-skymap-into-images.py
+++
@@ -1,7 +0,0 @@
-from cubemap_splitter import split_cubemap
-
-# Automatically determine format and create new directory with images at original image location
-# split_cubemap("data/cubemap.png")
-
-# Specify format and write to user defined directory
-split_cubemap("data/cubemap.png", format_type=1, output_directory="data/")
