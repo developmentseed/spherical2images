c8e3f43adb04a8726f528d71bf91ec222b5af82d
Rub21 <ruben@developmentseed.org>
2022-02-22 18:27:05 -0500

    remove side cube option

--- clip_pano.py
+++ clip_pano.py
@@ -12,31 +12,21 @@ import shutil
 import glob
 
 access_token = os.environ.get("MAPILLARY_ACCESS_TOKEN")
-sides_dict = {
-    "0": "front",
-    "1": "left",
-    "2": "right",
-    "3": "back",
-    "4": "over",
-    "5": "under",
-}
 
 
-def download_process_img(feature, output_images_path, image_clip_size, cube_sides):
+def download_process_img(feature, output_images_path, image_clip_size):
     """Download and clip the spherical image
 
     Args:
         feature (dict): feture
         output_images_path (str): path to save the images
         image_clip_size (int): Size of the image to clip
-        cube_sides (str): list of sited to clip
-
     Returns:
         [dict]: feature
     """
     sequence_id = feature["properties"]["sequence_id"]
     image_folder_path = f"{output_images_path}/{sequence_id}"
-
+    new_feature = None
     if not os.path.exists(image_folder_path):
         os.makedirs(image_folder_path)
 
@@ -49,8 +39,7 @@ def download_process_img(feature, output_images_path, image_clip_size, cube_side
 
     if not os.path.isfile(img_file_equirectangular):
         header = {"Authorization": "OAuth {}".format(access_token)}
-        url = "https://graph.mapillary.com/{}?fields=thumb_2048_url".format(
-            image_id)
+        url = "https://graph.mapillary.com/{}?fields=thumb_2048_url".format(image_id)
 
         try:
             r = requests.get(url, headers=header)
@@ -61,56 +50,30 @@ def download_process_img(feature, output_images_path, image_clip_size, cube_side
                 handler.write(image_data)
 
             # Convert Equirectangular -> Cubemap
-            cmd =f"convert360 --convert e2c --i {img_file_equirectangular}  --o {img_file_cubemap} --w {image_clip_size}"
+            cmd = f"convert360 --convert e2c --i {img_file_equirectangular}  --o {img_file_cubemap} --w {image_clip_size}"
             os.system(cmd)
 
             # Split Cubemap to simple images
             chumk_image_path = f"{image_folder_path}/{image_id}"
             if not os.path.exists(chumk_image_path):
                 os.makedirs(chumk_image_path)
-            split_cubemap(img_file_cubemap, format_type=1,
-                          output_directory=chumk_image_path)
+            split_cubemap(img_file_cubemap, format_type=1, output_directory=chumk_image_path)
             # Rename files
             clean_files(image_folder_path, image_id)
-
+            new_feature = feature
         except requests.exceptions.HTTPError as err:
             raise SystemExit(err)
-        except OSError as error : 
-            print(error)
+        except OSError as err:
+            raise SystemExit(err)
 
     else:
         print(f"File exist..{img_file_equirectangular}")
 
-    # # Check if chuck image files exist
-    # list_sides = []
-    # cube_list = cube_sides.split(",")
-    # for i in cube_list:
-    #     if i in sides_dict.keys():
-    #         img_cube_chunk = f"{image_folder_path}/{image_id}_{sides_dict[i]}.jpg"
-    #         if not os.path.isfile(img_cube_chunk):
-    #             list_sides.append(i)
-    #         else:
-    #             print(f"File exist..{img_cube_chunk}")
-
-    # # Split spherical image into chunks
-    # if len(list_sides) > 0:
-    #     # Convert Equirectangular -> Cubemap
-    #     os.system(
-    #         f"convert360 --convert e2c --i {img_file_equirectangular}  --o {img_file_cubemap} --w {image_clip_size}"
-    #     )
-
-    #     # Split Cubemap to simple images
-    #     chumk_image_path = f"{image_folder_path}/{image_id}"
-    #     if not os.path.exists(chumk_image_path):
-    #         os.makedirs(chumk_image_path)
-    #     split_cubemap(img_file_cubemap, format_type=1, output_directory=chumk_image_path)
-    #     clean_files(image_folder_path, image_id, list_sides)
-    return feature
+    return new_feature
 
 
 def clean_files(image_folder_path, image_id):
     chumk_image_path = f"{image_folder_path}/{image_id}"
-
     for file in glob.glob(f"{chumk_image_path}/*.jpg"):
         side = Path(file).stem
         os.rename(
@@ -118,47 +81,25 @@ def clean_files(image_folder_path, image_id):
             f"{image_folder_path}/{image_id}_{side}.jpg",
         )
 
-    # for side in list_sides:
-    #     os.rename(
-    #         f"{chumk_image_path}/{sides_dict[side]}.jpg",
-    #         f"{image_folder_path}/{image_id}_{sides_dict[side]}.jpg",
-    #     )
-    # # Remove files
-    # if os.path.exists(chumk_image_path):
-    #     shutil.rmtree(chumk_image_path)
 
-
-def process_image(input_points, output_images_path, image_clip_size, cube_sides):
+def process_image(input_points, output_images_path, image_clip_size):
 
     with open(input_points, "r", encoding="utf8") as f:
         features = json.load(f)["features"]
 
     # Process in parallel
     results = Parallel(n_jobs=-1)(
-        delayed(download_process_img)(
-            feature, output_images_path, image_clip_size, cube_sides)
+        delayed(download_process_img)(feature, output_images_path, image_clip_size)
         for feature in tqdm(features, desc=f"Processing images for...", total=len(features))
     )
     return features
 
 
-@click.command(short_help="Script to get last updates for adapters")
+@click.command(short_help="Script to conver 360 images to simple sides images")
 @click.option(
     "--input_points",
     help="input points geojson file",
     default="data/input_points.geojson",
-)
-@click.option(
-    "--cube_sides",
-    help="""Select which sides  need to be extracted: 
-        - 0:front
-		- 1:left
-		- 2:right
-		- 3:back
-		- 4:over
-		- 5:under""",
-    default="1,2",
-    type=str,
 )
 @click.option("--image_clip_size", help="Image size of the image to clip", default=512)
 @click.option(
@@ -171,9 +112,8 @@ def process_image(input_points, output_images_path, image_clip_size, cube_sides)
     help="output points for images that were processed",
     default="data/output_points.geojson",
 )
-def main(input_points, output_points, output_images_path, image_clip_size, cube_sides):
-    output = process_image(input_points, output_images_path,
-                           image_clip_size, cube_sides)
+def main(input_points, output_points, output_images_path, image_clip_size):
+    output = process_image(input_points, output_images_path, image_clip_size)
     with open(output_points, "w") as f:
         json.dump({"type": "FeatureCollection", "features": output}, f)
 
